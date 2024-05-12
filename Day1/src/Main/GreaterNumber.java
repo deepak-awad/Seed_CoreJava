e8459575a8709e5ec8536fd0befd3cb60a91df82
deepak-awad <awaddeepak8448@gmail.com>
2024-05-12 20:38:57 +0530

    Adding programs simple

---
+++ Day1/src/Main/GreaterNumber.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class GreaterNumber {
+	
+	public static void main(String args[])
+	{
+		int n1=10, n2=5;
+		if(n1 > n2)
+		{
+			System.out.println(n1+" is greater than "+n2);
+		}else
+		{
+			System.out.println(n2+" is greater than "+n1);
+		}
+	}
+
+}
