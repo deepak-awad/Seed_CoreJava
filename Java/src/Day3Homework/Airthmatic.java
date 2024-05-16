4be1d7ea61941c5cbfe68be9e0bf0c201560b361
deepak-awad <awaddeepak8448@gmail.com>
2024-05-16 19:25:34 +0530

    Day3 Homweork

---
+++ Java/src/Day3Homework/Airthmatic.java
@@ -0,0 +1,14 @@
+package Day3Homework;
+
+public class Airthmatic {
+	
+	public static void add(int ...a)
+	{
+		for(int i : a)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ Java/src/Day3Homework/Calculator.java
@@ -0,0 +1,10 @@
+package Day3Homework;
+
+public class Calculator {
+	
+	public static void main(String args[])
+	{
+		Airthmatic.add(10,20,30);
+	}
+
+}
